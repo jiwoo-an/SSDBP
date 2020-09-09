e09eb44a8f475d1e9afb3ef3230ed7bde45095e6
HHHong10 <48639017+HHHong10@users.noreply.github.com>
2020-09-10 00:11:39 +0900

    index.php
    2주차 실습 과제

---
+++ W02-P/index.php
@@ -0,0 +1,40 @@
+<?php
+  $link = mysqli_connect('localhost','root','ghd1006','fav');
+  $query = "SELECT * FROM film";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while($row = mysqli_fetch_array($result)){
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'mainactors' => 'I love 90s movie because of the mood. Let me introduce my favorite 90s high-teen film!<br>
+    The order is meaningless, I love these equally♡'
+  );
+
+  if(isset($_GET['id']) ) {
+    $query = "SELECT * FROM film WHERE id={$_GET['id']}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+      'title' => $row['title'],
+      'mainactors' => $row['mainactors']
+    );
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> 90s High-Teen Film </title>
+</head>
+<body>
+  <h1><a href="index.php">Favorite 90s High-Teen Film</h1>
+  <ul> <?= $list ?> </ul>
+  <a href="create.php">create</a>
+  <h2> <?= $article['title']?> </h2>
+   <?= $article['mainactors'] ?>
+</body>
+</html>
