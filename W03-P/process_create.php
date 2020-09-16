c5f51285808ad4446a6505c8f1de336a900709bc
HHHong10 <48639017+HHHong10@users.noreply.github.com>
2020-09-16 23:13:31 +0900

    w03-p
    week 03 assignment

---
+++ W03-P/process_create.php
@@ -0,0 +1,22 @@
+<?php
+  $link = mysqli_connect("localhost", "root", "ghd1006", "fav");
+  $filtered = array(
+    'title' => mysqli_real_escape_string($link, $_POST['title']),
+    'mainactors' => mysqli_real_escape_string($link, $_POST['mainactors'])
+  );
+  $query = "
+    INSERT INTO film (
+      title, mainactors
+      ) VALUE (
+        '{$filtered['title']}',
+        '{$filtered['mainactors']}'
+        )
+  ";
+
+  $result = mysqli_multi_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관라자에게 문의하세요.';
+      echo mysqli_error($link);
+  } else {
+      echo '성공했습니다. <a href="index.php">돌아가기</a>';
+  }
