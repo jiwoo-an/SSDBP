a3917123405b1d127bd62640276ee9e72cd7f84c
HHHong10 <48639017+HHHong10@users.noreply.github.com>
2020-09-29 00:01:00 +0900

    process_update_director.php

---
+++ W04-P/process_update_director.php
@@ -0,0 +1,21 @@
+<?php
+  $link = mysqli_connect("localhost", "root", "ghd1006", "fav");
+  $filtered = array(
+    'id' => mysqli_real_escape_string($link, $_POST['id']),
+    'name' => mysqli_real_escape_string($link, $_POST['name'])
+  );
+  $query = "
+    UPDATE director
+    SET
+      name = '{$filtered['name']}'
+    WHERE
+      id = '{$filtered['id']}'
+  ";
+
+  $result = mysqli_query($link, $query);
+  if ($result == false) {
+      echo '수정하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      errror_log(mysqli_error($link));
+  } else {
+      header('Location: director.php?id='.$filtered['id']);
+  }
