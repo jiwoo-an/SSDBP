73175a7cd8ab28492089576a10af2d49cfa35d6e
HHHong10 <48639017+HHHong10@users.noreply.github.com>
2020-09-29 00:01:21 +0900

    update.php

---
+++ W04-P/update.php
@@ -0,0 +1,44 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'ghd1006', 'fav');
+  $query = "SELECT * FROM film";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'mainactors' => 'I love 90s movie because of the mood. Let me introduce my favorite 90s high-teen film!<br>
+    The order is meaningless, I love these equally♡'
+  );
+
+  if (isset($_GET['id'])) {
+      $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+      $query = "SELECT * FROM film WHERE id={$filtered_id}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+        'title' => $row['title'],
+        'mainactors' => $row['mainactors']
+      );
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> 90s High-Teen Film </title>
+</head>
+<body>
+  <h1><a href="index.php">Favorite 90s High-Teen Film</a></h1>
+  <ul> <?= $list ?> </ul>
+  <form action="process_update.php" method="post">
+    <input type="hidden" name="id" value="<?=$filtered_id ?>">
+    <p><input type="text" name="title" placeholder="title" value="<?=$article['title'] ?>"></p>
+    <p><textarea name="mainactors" placeholder="mainactors"><?=$article['mainactors'] ?></textarea></p>
+    <p><input type="submit"></p>
+  </form>
+</body>
+</html>
