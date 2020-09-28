f3f2bd80fd2b091382c05ffd6b6b89bbf113ec37
HHHong10 <48639017+HHHong10@users.noreply.github.com>
2020-09-28 23:59:02 +0900

    process_create_director.php

---
+++ W04-P/process_create_director.php
@@ -0,0 +1,20 @@
+<?php
+  $link = mysqli_connect("localhost", "root", "ghd1006", "fav");
+  $filtered = array(
+    'name' => mysqli_real_escape_string($link, $_POST['name'])
+  );
+  $query = "
+    INSERT INTO director (
+      name
+      ) VALUES (
+        '{$filtered['name']}'
+        )
+  ";
+
+  $result = mysqli_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      errror_log(mysqli_error($link));
+  } else {
+      header('Location: director.php');
+  }
